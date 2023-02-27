1d71de9bd01120bc4b258b35691d13d0bbffa36c
Wansaaa <ichwansanusi60@gmail.com>
2023-02-27 23:04:53 +0700

    Belajar Tailwind CSS Portfolio

---
+++ dist/js/script.js
@@ -0,0 +1,44 @@
+// Navbar Fixed
+window.onscroll = function () {
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top');
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
+  } else {
+    header.classList.remove('navbar-fixed');
+    toTop.classList.remove('flex');
+    toTop.classList.add('hidden');
+  }
+};
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
+});
+
+// Klik di luar hamburger
+window.addEventListener('click', function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
+});
+
+// Overlay
+const elementclose = document.getElementsByClassName('overlay');
+
+for (let i = 0; i < elementclose.length; i++) {
+  elementclose[i].addEventListener('click', function () {
+    let url = window.location.href.substr(0, window.location.href.indexOf('#boostrap'));
+    window.location = url;
+  });
+  document.location.href = 'index.html#portfolio';
+}
